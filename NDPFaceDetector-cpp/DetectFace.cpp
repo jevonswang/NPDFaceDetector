1dd347a6f567993203d9cbc821317e9cd4ed30d9
jevonswang <972562104@qq.com>
2015-11-27 09:33:37 +0800

    code

--- NDPFaceDetector-cpp/DetectFace.cpp
+++ NDPFaceDetector-cpp/DetectFace.cpp
@@ -1,5 +1,11 @@
 #include "DetectFace.h"
 
+bool Logistic(arma::uvec &candi_rects_score, arma::uvec &weight){
+	for (int i = 0; i < candi_rects_score.n_elem; i++){
+		weight[i] = log(1 + exp(candi_rects_score[i]));
+	}
+}
+
 bool DetectFace(
 	vector<cv::Rect> &rects, 
 	NPDModel &npdModel, 
@@ -25,7 +31,7 @@ bool DetectFace(
 	//cout << grayImg(cv::Range(0, 10), cv::Range(0, 10)) << endl;
 
 	// get candidate rects
-	vector<Candi_rects> candi_rects;
+	arma::mat candi_rects;// col[0]:row,col[1]:col,col[2]:size,col[3]:score 
 	NPDScan(candi_rects,npdModel, armaImg, minFace, maxFace, numThreads);
 
 	
@@ -38,13 +44,78 @@ bool DetectFace(
 	arma::mat predicate = arma::eye<arma::mat>(numCandidates,numCandidates);
 	// i and j belong to the same group if predicate(i,j) = 1
 
+	// mark nearby detections
 	int h, w, s;
 	for (int i = 0; i < numCandidates; i++){
 		for (int j = 0; j < numCandidates; j++){
+			
+			h = min(candi_rects(i,0) + candi_rects(i,2), candi_rects(j,0) + candi_rects(j,2))
+				- max(candi_rects(i,0), candi_rects(j,0));
+			w = min(candi_rects(i,1) + candi_rects(i,2), candi_rects(j,1) + candi_rects(j,2))
+				- max(candi_rects(i,1), candi_rects(j,1));
+			s = max(h, 0) * max(w, 0);
 
+			if ((s / candi_rects(i,2) * candi_rects(i,2) + candi_rects(j,2) * candi_rects(j,2) - s) >= overlappingThreshold){
+				predicate(i, j) = 1;
+				predicate(j, i) = 1;
+			}
 		}
 	}
+
+	// merge nearby detections
+	arma::mat label;
+	Partition(predicate,label,numCandidates);
+
+	arma::uvec rects_row, rects_col, rects_size, rects_score, rects_neighbors;
+	rects_row.zeros(numCandidates, 1);
+	rects_col.zeros(numCandidates, 1);
+	rects_size.zeros(numCandidates, 1);
+	rects_score.zeros(numCandidates, 1);
+	rects_neighbors.zeros(numCandidates, 1);
+
+
+	for (int i = 0; i < numCandidates; i++){
+		arma::uvec index = arma::find(label == i);
+
+		arma::uvec candi_rects_score(index);
+		for (int j = 0; j < index.n_elem; j++){
+			candi_rects_score[j] = candi_rects[j].score;
+		}
+		
+		candi_rects(index).col(5);
+
+
+		arma::uvec weight(candi_rects);
+		Logistic(candi_rects_score, weight);
+		rects_score[i] = arma::sum(weight);
+		rects_neighbors[i] = index.n_elem;
+
+		if (0 == rects_score[i]){
+			int n_elem = weight.n_elem;
+			weight.ones();
+			weight = weight / n_elem;
+		}
+		else{
+			weight = weight / arma::sum(weight);
+		}
+
+		rects_size[i] = floor([candi_rects(index).size] * weight);
+		rects_col[i] = floor(([candi_rects(index).col] + [candi_rects(index).size] / 2) * weight - rects(i).size / 2);
+		rects_row[i] = floor(([candi_rects(index).row] + [candi_rects(index).size] / 2) * weight - rects(i).size / 2);
+	}
+
+
+
+
+
+
+
+
+
+
+
 
 
 	return true;
 }
+
--- NDPFaceDetector-cpp/DetectFace.h
+++ NDPFaceDetector-cpp/DetectFace.h
@@ -3,9 +3,12 @@
 #include <armadillo>
 #include <opencv2/opencv.hpp>
 #include <vector>
+#include <algorithm>
+#include <cmath>
 
 #include "structs.h"
 #include "NPDScan.h"
+#include "Partition.h"
 
 using namespace std;
 
